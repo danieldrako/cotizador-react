c074b97e9bfbd431468d9f7928919c33ccdad525
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-04-09 23:13:39 -0600

    after pass data and functions to provider

--- src/context/CotizadorProvider.jsx
+++ src/context/CotizadorProvider.jsx
@@ -3,11 +3,13 @@ import { createContext } from "react";
 const CotizadorContext = createContext()
 
 const CotizadorProvider = ({children}) => { //*provider=fuente de los datos
+    //? Espacio para funciones y hooks
+    const hola = 'hola mundo'
 
     return(
         <CotizadorContext.Provider
             value={{
-                
+                hola: hola
             }}
         >
             {children}
