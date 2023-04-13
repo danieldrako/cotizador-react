3cb465b37792f8bf549e015da3ef9a457e7bc96c
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-04-13 14:43:34 -0600

    after adding more information to the result

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,12 +1,25 @@
 import useCotizador from "../hooks/useCotizador"
+import { MARCAS, PLANES } from "../constants"
 
 const Resultado = () => {
-    const {resultado} = useCotizador()
+    const {resultado, datos} = useCotizador()
+    const {marca, plan, year} = datos
+
+    const [ nombreMarca ] = MARCAS.filter(m=>m.id === Number(marca) )
 
     if(resultado === 0) return null
-    
+
   return (
-    <div>{resultado}</div>
+    <div className="bg-yellow-300">
+        <h2 className="text-gray-700">
+            Resumen
+        </h2>
+
+        <p>
+            <span>Marca: </span>
+            {nombreMarca.nombre}
+        </p>
+    </div>
   )
 }
 
